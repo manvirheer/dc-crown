22d663478d71b4ace641ed3dd4e8b77cc4450196
Manvir Singh Heer <40204204+manvirheer@users.noreply.github.com>
2020-10-12 00:15:31 -0700

    Update Files

--- public/js/fireConfig.js
+++ public/js/fireConfig.js
@@ -1,3 +1,8 @@
+const txtEmail = document.querySelector("#txtEmail");
+const txtPassword = document.querySelector("#txtPassword");
+const btnLogin = document.querySelector("#btnLogin");
+const btnSignUp = document.querySelector("#btnSignUp");
+
 const firebaseConfig = {
     apiKey: "AIzaSyATcKT0lCZyOcKTxOHXSpI4d1bZs1ynyVQ",
     authDomain: "dc-crown.firebaseapp.com",
@@ -8,19 +13,30 @@ const firebaseConfig = {
     appId: "1:555456365972:web:803eb1ab8987013581fb81",
     measurementId: "G-Y03F6FRG7Q"
 };
+
 //JLKJADSK
 firebase.initializeApp(firebaseConfig);
 
-const txtEmail = document.querySelector("#txtEmail");
-const txtPassword = document.querySelector("#txtPassword");
-const btnLogin = document.querySelector("#btnLogin");
 
 btnLogin.addEventListener("click", e => {
     e.preventDefault();
+    const auth = firebase.auth();
+    checkLoggedInUser();
     const email = txtEmail.value;
     const password = txtPassword.value;
-    const auth = firebase.auth();
     const promise = auth.signInWithEmailAndPassword(email, password);
+    promise.catch(e => console.log(e.message));
+
+})
+
+btnSignUp.addEventListener("click", e => {
+    e.preventDefault();
+    const auth = firebase.auth();
+    console.log(auth);
+    const email = txtEmail.value;
+    console.log("Manivr")
+    const password = txtPassword.value;
+    const promise = auth.createUserWithEmailAndPassword(email, password);
     promise.catch(e => console.log(e.message));
 
 })
